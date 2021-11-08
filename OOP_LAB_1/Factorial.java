79c46016afdae53cdc9e8e6a1799ebfc4cfa33e1
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2021-11-08 09:53:47 +0530

    LAB_1_Files

---
+++ OOP_LAB_1/Factorial.java
@@ -0,0 +1,20 @@
+package factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+    
+    static int factorial(int n){
+        if(n < 2){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        num = sc.nextInt();
+        System.out.println("Factorial of the Number is : " + factorial(num));
+    }
+}
