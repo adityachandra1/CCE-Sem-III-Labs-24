c89f43786b319fef93455a9eae49b22f4c239261
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2021-11-09 17:48:44 +0530

    added Lab 1_3

--- DS Lab_1/Lab1_3.cpp
+++ DS Lab_1/Lab1_3.cpp
@@ -1,15 +1,12 @@
-
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
 #define N 100
 
 class time{
-    private:
-    int hrs, minutes, seconds;
     public:
+    int hrs, minutes, seconds;
     void display(){
         cout << "Time : " << hrs << " hrs " << minutes << " mins " << seconds << " seconds" << endl;
     }
@@ -22,12 +19,24 @@ class time{
     }
 };
 
-time diff;
+time diff(time t1, time t2){
+    time t3;
+    t3.hrs = (t1.hrs - t2.hrs) < 0 ? (t2.hrs - t1.hrs)  : (t1.hrs - t2.hrs) ;
+    t3.minutes = (t1.minutes - t2.minutes) < 0 ? (t2.minutes - t1.minutes) : (t1.minutes - t2.minutes);
+    t3.seconds = (t1.seconds - t2.seconds) < 0 ? (t2.seconds - t1.seconds) : (t1.seconds - t2.seconds);
+    return t3;
+}
+
 
 
 int main(){
-   time t;
+   time t1 , t2;
    cout << "Enter Time" << endl;
-   t.read();
-   t.display();
+   t1.read();
+   t2.read();
+   t1.display();
+   t2.display();
+
+   time t3 = diff(t1, t2);
+   t3.display();
 }
