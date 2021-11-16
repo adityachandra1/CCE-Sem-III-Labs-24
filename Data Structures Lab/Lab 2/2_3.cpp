606674f5b05f6d0825ecf5d55d385ea3e3bbc022
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2021-11-16 17:47:12 +0530

    Lab 2

---
+++ Data Structures Lab/Lab 2/2_3.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+#define MAX_SIZE 100
+#define ERROR -400
+
+class stack {
+    public:
+    int top;
+    int a[MAX_SIZE];
+
+
+    stack() { top = -1; }
+    bool isEmpty() { return top == -1; }
+    bool isFull() { return top == MAX_SIZE - 1; }
+    void push(int ele) {
+        if (isFull()) cout << "Stack-Overflow" << endl;
+        else a[++top] = ele;
+    }
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack-Underflow" << endl;
+            return ERROR;
+        } else {
+            return a[top--];
+        }
+    }
+    void display(){
+        if (isEmpty()) cout << "Empty-Stack" << endl;
+        else{
+            for (int i = 0; i <= top; i++){
+                cout << a[i] << " ";
+            }
+        }
+        cout << endl;
+    }
+};
+
+int main(){
+    int num;
+    cout << "enter decimal number" << endl;
+    cin >> num;
+    int temp = num;
+    int base;
+    cout << "enter base" << endl;
+    cin >> base;
+
+    stack s;
+    while(temp!=0){
+     s.push(temp%base);
+     temp /= base;
+    }
+    cout << char(69) << endl;
+    s.display();
+    cout << num << " in base " << base  << " is"  << endl;
+
+    while(!s.isEmpty()){
+        if(s.top < 10)
+            cout << s.pop();
+        if(s.top >= 10){
+            int x = s.pop();
+            cout << char(x + 55) << endl;
+        }
+    }
+    cout << endl;
+
+}
---
+++ Data Structures Lab/Lab 2/balancedParenthesis.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_SIZE 100
+#define ERROR -400
+
+//check palindrome
+class stack {
+    public:
+    int top;
+    char a[MAX_SIZE];
+
+    stack() { top = -1; }
+    bool isEmpty() { return top == -1; }
+    bool isFull() { return top == MAX_SIZE - 1; }
+
+    void push(char ele) {
+        if (isFull()) cout << "Stack-Overflow" << endl;
+        else a[++top] = ele;
+    }
+    char pop() {
+        if (isEmpty()) {
+            cout << "Stack-Underflow" << endl;
+            return ERROR;
+        } else {
+            return a[top--];
+        }
+    }
+    void display(){
+        if (isEmpty()) cout << "Empty-Stack" << endl;
+        else{
+            for (int i = 0; i <= top; i++){
+                cout << a[i] << " ";
+            }
+        }
+        cout << endl;
+    }
+};
+
+int main(){
+    stack s; // stack of characters
+    char str[MAX_SIZE];
+    cout << "Enter the string" << endl;
+    cin >> str;
+    int length = 0;
+    for(int i = 0 ; str[i]!= '\0' ; i++){
+        length++;
+    }
+    cout << "length of the string: ";
+    cout << length << endl;
+
+    for(int i = 0 ; i < length ; i++){
+        if(str[i] == '(' || str[i] == '{' || str[i] == '['){
+            s.push(str[i]);
+        }
+        if(str[i] == ')' || str[i] == '}' || str[i] == ']'){
+            if(s.isEmpty()){
+                cout << "Unbalanced Parenthesis" << endl;
+                return -1;
+                break;
+            };
+            if(s.top == '('){
+                s.pop();
+                continue;
+            }
+            if(s.top == '{'){
+                s.pop();
+                continue;
+            }
+            if(s.top == '{'){
+                s.pop();
+                continue;
+            }
+        }
+        s.display();
+    }
+
+    s.isEmpty()? cout << "Balanced parenthesis" : cout << "Unbalanced Parenthesis" << endl;
+}
