0f62767b6a05f4cf7e7fc3aa6764e80fd35b4f3f
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2021-11-30 17:54:56 +0530

    Lab 4

---
+++ Data Structures Lab/Lab 4/Lab_4_5.cpp
@@ -0,0 +1,152 @@
+
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+#define MAX_SIZE 50
+enum precedence {lparen, rparen, pls, mnus, times, divide, mod, eos, operand};
+
+precedence get_token (char c){
+   switch(c){
+   case '(' : return lparen;
+   case ')' : return rparen;
+   case '+' : return pls;
+   case '-' : return mnus;
+   case '*' : return times;
+   case '/' : return divide;
+   case '%' : return mod;
+   case '#' : return eos;
+   default : return operand;
+   }
+}
+
+int prec(char c) {
+    if(c == '^')
+        return 3;
+    else if(c == '/' || c=='*')
+        return 2;
+    else if(c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+
+class stack {
+    public:
+    int top;
+    int a[MAX_SIZE];
+
+    stack() { top = -1; }
+    bool isEmpty() { return top == -1; }
+    bool isFull() { return top == MAX_SIZE - 1; }
+
+    void push(int ele) {
+        if (isFull()) cout << "Stack-Overflow" << endl;
+        else a[++top] = ele;
+    }
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack-Underflow" << endl;
+            return -999999;
+        } else {
+            return a[top--];
+        }
+    }
+    void display(){
+        if (isEmpty()) cout << "Empty-Stack" << endl;
+        else{
+            for (int i = 0; i <= top; i++){
+                cout << a[i] << " ";
+            }
+        }
+        cout << endl;
+    }
+};
+
+void infix2postfix(char infix[]){
+    stack st;
+    char postfix[50] = {0};
+    precedence temp;
+    int curr = 0; // curr ptr for postfix;
+    char c;
+
+    for(int i = 0 ; infix[i] != '\0' ; i++){
+        c = infix[i];
+        cout << postfix;
+        if( c >= 'a' && c <= 'Z' || c >= 'A' && c <= 'Z'|| c >= '0' && c <= '9'){
+            postfix[curr++] = c;
+             cout << postfix;
+        }
+
+        else if(c == '(') st.push('(');
+        else if(c == ')'){
+            while(st.top != '(') postfix[curr++] = st.pop();
+            st.pop();
+        }else{
+            while(!st.isEmpty() && prec(infix[i]) <= prec(st.top)){
+                postfix[curr++] = st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    while(!st.isEmpty()){
+        postfix[curr++] = st.pop();
+    }
+
+    cout << postfix << endl;
+
+}
+
+int evaluatePostfix(char str[]){
+    stack st;
+    for(int i = 0 ; str[i] != '\0'; i++){
+        if(str[i] >= '0' && str[i] <= '9'){
+            st.push(str[i] - '0');
+        }else{
+            int val1 = st.pop();
+            int val2 = st.pop();
+            switch(str[i]){
+                case '+': st.push(val2 + val1); break;
+                case '-': st.push(val2 - val1); break;
+                case '*': st.push(val2 * val1); break;
+                case '/': st.push(val2 / val1); break;
+                case '%': st.push(val2 % val1) ; break;
+            }
+        }
+    }
+    return st.pop();
+}
+
+int evaluatePrefix(char str[]){
+    stack st;
+    int i = 0;
+    while(str[i] != '\0'){
+        i++;
+    }
+    i--;
+    while(i > -1){
+        if(str[i] >= '0' && str[i] <= '9'){
+            st.push(str[i] - '0');
+        }else{
+            int val1 = st.pop();
+            int val2 = st.pop();
+            switch(str[i]){
+                case '+': st.push(val2 + val1); break;
+                case '-': st.push(val2 - val1); break;
+                case '*': st.push(val2 * val1); break;
+                case '/': st.push(val2 / val1); break;
+                case '%': st.push(val2 % val1) ; break;
+            }
+        }
+        i--;
+    }
+    return st.pop();
+}
+
+int main(){
+    char prefix[100];
+    cin >> prefix;
+    cout << evaluatePrefix(prefix);
+}
