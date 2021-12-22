8e7baba046d3a26f0941ac6be7cd67d4e31f644e
Aditya Chandra <adityachandra9898@gmail.com>
2021-12-23 01:52:00 +0530

    lab-3

--- Data Structures Lab/Lab 3/Lab3_1.cpp
+++ Data Structures Lab/Lab 3/Lab3_1.cpp
@@ -3,34 +3,40 @@ using namespace std;
 
 #define MAX_SIZE 100
 
-class queue{
+class queue {
     int front;
     int rear;
     int q[MAX_SIZE];
 
-public:
-    queue(){
-        front = 0 ;
+   public:
+    queue() {
+        front = 0;
         rear = -1;
     }
 
-    int frontElement(){
+    int frontElement() {
         return q[front];
     }
 
-    void insert(int ele){
-        if (rear == MAX_SIZE - 1) cout << "Queue Overflow" << endl;
-        else q[++rear] = ele;
+    void insert(int ele) {
+        if (rear == MAX_SIZE - 1)
+            cout << "Queue Overflow" << endl;
+        else
+            q[++rear] = ele;
     }
-    int deleteq (){
-        if (front > rear) return -1;
-        else q[front++];
+    int deleteq() {
+        if (front > rear) {
+            cout << "Queue Empty" << endl;
+            return -1;
+        } else
+            return q[front++];
     }
-    void display(){
-        if(front > rear) cout << "Queue Empty" << endl;
-        else{
-            cout << "Queue" << endl;
-            for(int i = front ; i <= rear ; i++){
+    void display() {
+        if (front > rear)
+            cout << "Queue Empty" << endl;
+        else {
+            cout << "Queue : ";
+            for (int i = front; i <= rear; i++) {
                 cout << q[i] << " ";
             }
         }
@@ -38,14 +44,40 @@ public:
     }
 };
 
-int main(){
+int main() {
     queue qu;
     qu.insert(10);
     qu.insert(20);
     qu.insert(30);
-    cout << qu.frontElement() << endl;
     qu.display();
-    qu.deleteq();
-    qu.deleteq();
-    qu.display();
+
+    int opt, flag = 0;
+
+    cout << "Enter 1 for inserting an element\n 2 for deleting an element\n 3 for getting the top element\n 4 to display the queue\n 5 to exit\n"
+         << endl;
+    while (flag != 1) {
+        cout << "Enter Option" << endl;
+        cin >> opt;
+        switch (opt) {
+            case 1:
+                int x;
+                cout << "Enter the element to be inserted" << endl;
+                cin >> x;
+                qu.insert(x);
+                break;
+            case 2:
+                cout << "Element Deleted " << qu.deleteq() << endl;
+                break;
+            case 3:
+                cout << "Top Element in the queue: " << qu.frontElement() << endl;
+                break;
+            case 4:
+                qu.display();
+                break;
+            case 5:
+                flag = 1;
+                break;
+        }
+        cout << endl;
+    }
 }
--- Data Structures Lab/Lab 3/Lab3_2.cpp
+++ Data Structures Lab/Lab 3/Lab3_2.cpp
@@ -3,55 +3,46 @@ using namespace std;
 
 #define MAX_SIZE 100
 
-
-class spm
-{
-public:
+class spm {
+   public:
     int row, col, val;
 
-    void set(int r, int c, int v)
-    {
+    void set(int r, int c, int v) {
         row = r;
         col = c;
         val = v;
     }
 
-    void fast_transpose(spm a[], spm b[])
-    {
-        int r=a[0].row, c=a[0].col, v=a[0].val;
-        int frequency[c]= {0};
-        int start[c];
-        b[0].set(a[0].col,a[0].row,a[0].val);
-        if(v>0)
-        {
-            for(int i=1; i<=v; i++)
-            {
-                frequency[a[i].col]++;
-            }
-            start[0]=1;
-            for(int i=1; i<c; i++)
-            {
-                start[i]=start[i-1]+frequency[i-1];
-            }
+    void fast_transpose(spm a[], spm b[]) {
+        int r = a[0].row, c = a[0].col, v = a[0].val;
+        int total[c] = {0};
+        int index[c + 1], col_no;
+        b[0].set(a[0].col, a[0].row, a[0].val);
 
-            for(int i=0; i<=v; i++)
-            {
-                int j=start[a[i].col]+1;
-                b[j++].set(a[i].col,a[i].row,a[i].val);
-            }
+        for (int i = 1; i <= v; i++) {
+            col_no = a[i].col;
+            total[col_no]++; 
+        }
+        index[0] = 1;
+
+        for (int i = 1; i <= c; i++) {
+            index[i] = index[i - 1] + total[i - 1];
+        }
+
+        for (int i = 1; i <= v; i++) {
+            col_no = a[i].col;
+            int loc = index[col_no];
+            b[loc].set(a[i].col, a[i].row, a[i].val);
+            index[col_no]++;
         }
     }
 
-    void display()
-    {
-        cout<<row<<' '<<col<<' '<<val<<" "<<endl;
+    void display() {
+        cout << row << ' ' << col << ' ' << val << " " << endl;
     }
 };
 
-
-
-int main()
-{
+int main() {
     spm a[MAX_SIZE];
 
     int m, n;
@@ -62,34 +53,28 @@ int main()
     int cnt = 0, k = 0;
     cout << "Enter the elements in the matrix" << endl;
 
-    for(int i = 0 ; i < m ; i++)
-    {
-        for(int j = 0 ; j < n ; j++)
-        {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> mat[i][j];
-            if(mat[i][j]!= 0)
-            {
-                a[++cnt].set(i,j,mat[i][j]);
+            if (mat[i][j] != 0) {
+                a[++cnt].set(i, j, mat[i][j]);
             }
         }
     }
 
     a[0].val = cnt;
 
-    cout<<"Matrix before Transposing"<<endl;
+    cout << "Matrix before Transposing" << endl;
 
-    for(int i=0; k<=cnt; k++)
-    {
+    for (int i = 0; k <= cnt; k++) {
         a[k].display();
     }
-    spm b[cnt+1], temp;
+    spm b[cnt + 1], temp;
 
-    cout<<"Matrix after Transposing"<<endl;
+    cout << "Matrix after Transposing" << endl;
 
-    temp.fast_transpose(a,b);
-    for(int k=0; k<cnt; k++)
-    {
+    temp.fast_transpose(a, b);
+    for (int k = 0; k <= cnt; k++) {
         b[k].display();
     }
 }
-
