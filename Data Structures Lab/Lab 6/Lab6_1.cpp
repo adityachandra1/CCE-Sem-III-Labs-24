cf3b1d49b57247b64f1d2fe1104c688c46442d0d
Aditya Chandra <adityachandra9898@gmail.com>
2022-01-08 01:57:13 +0530

    lab-6

--- Data Structures Lab/Lab 6/Lab6_1.cpp
+++ Data Structures Lab/Lab 6/Lab6_1.cpp
@@ -1,96 +1,225 @@
 #include <iostream>
-
 using namespace std;
 
-class Node{
-    int val;
-    Node * next;
-    Node * prev;
-
-    Node(int x){
-        val = x;
-        prev = NULL;
-        next = NULL;
-    }
+class Node {
+   public:
+    int data;
+    Node *next, *prev;
 };
 
-class doublyLinkedList{
-public:
-    Node* head;
-    doublyLinkedList{
+class DoublyLinkedList {
+   private:
+    Node *head, *tail;
+
+   public:
+    DoublyLinkedList() {
         head = NULL;
+        tail = NULL;
     }
 
-    void insert_rear(int x){
-        Node* node = new Node(x);
-        Node*temp = head;
-        while (temp -> next){
-            temp = temp -> next;
+    //1
+    void insert_end(int elem) {
+        Node *temp = new Node;
+        temp->data = elem;
+        temp->next = NULL;
+        if (head == NULL) {
+            // list is empty
+            temp->prev = NULL;
+            head = temp;
+        } else {
+            temp->prev = tail;
+            tail->next = temp;
         }
-        temp -> next = node;
-        node -> prev = temp;
-        node -> next = NULL;
+        tail = temp;
     }
 
-    void delete_rear(){
-         Node*temp = head;
-        while (temp -> next -> next){
-            temp = temp -> next;
+    //2
+    void delete_end() {
+        Node *temp = tail;
+        if (temp->prev == NULL) {
+            head = tail = NULL;
+            delete temp;
+            return;
         }
-        Node* deleted = temp -> next
-        deleted -> prev = NULL;
-        temp -> next = NULL;
-        delete(deleted);
+        temp->prev->next = NULL;
+        tail = temp->prev;
+        delete temp;
+        return;
     }
 
-    void insert_at_pos(int x, int pos){
-        Node* node = new Node(x);
-        Node* temp = head;
-        int curr = 0;
-        while(curr < pos-1 && temp -> next){
-            temp = temp -> next;
-            curr++;
+    //3
+    void insert_pos(int elem, int pos) {
+        Node *temp = new Node;
+        Node *trav = new Node;
+        temp->data = elem;
+        trav = head;
+        int count = 0;
+        while (trav != NULL) {
+            if (count == pos) {
+                break;
+            }
+            count++;
+            trav = trav->next;
         }
-        node ->
+        if (trav == NULL) {
+            insert_end(elem);
+            return;
+        }
+        trav->prev->next = temp;
+        temp->prev = trav->prev;
+        temp->next = trav;
+        trav->prev = temp;
     }
 
-    void delete_at_pos(int pos){
-        Node* node = new Node(x);
-        Node* temp = head;
-        int curr = 1;
-        while(curr < pos-1 && temp -> next){
-            temp = temp -> next;
-            curr++;
+    //4
+    void delete_pos(int pos) {
+        Node *trav;
+        trav = head;
+        if (pos == 0) {
+            head = trav->next;
+            delete trav;
+            return;
+        }
+        int count = 0;
+        while (trav != NULL) {
+            if (count == pos) {
+                break;
+            }
+            count++;
+            trav = trav->next;
+        }
+        if (trav == NULL) {
+            delete_end();
+        }
+        trav->prev->next = trav->next;
+        trav->next->prev = trav->prev;
+    }
+
+    //5
+    void insert_after(int elem, int val) {
+        Node *temp = new Node;
+        Node *trav = new Node;
+        temp->data = elem;
+        trav = head;
+        bool found = false;
+        while (trav != NULL) {
+            if (val == trav->data) {
+                found = true;
+                if (trav->next != NULL)
+                    trav->next->prev = temp;
+                else
+                    tail = temp;
+                temp->next = trav->next;
+                trav->next = temp;
+                temp->prev = trav;
+                break;
+            }
+            trav = trav->next;
+        }
+        if (!found) {
+            cout << "Element not found" << endl;
         }
     }
 
-    void insert_after(int x, int pos){
-         Node* node = new Node(x);
-        Node* temp = head;
-        int curr = 1;
-        while(curr < pos && temp -> next){
-            temp = temp -> next;
-            curr++;
+    //6
+    void insert_before(int elem, int val) {
+        Node *temp = new Node;
+        Node *trav = new Node;
+        temp->data = elem;
+        trav = tail;
+        bool found = false;
+        while (trav != NULL) {
+            if (val == trav->data) {
+                found = true;
+                if (trav->prev != NULL)
+                    trav->prev->next = temp;
+                else
+                    head = temp;
+                temp->prev = trav->prev;
+                trav->prev = temp;
+                temp->next = trav;
+                break;
+            }
+            trav = trav->prev;
+        }
+        if (!found) {
+            cout << "Element not found" << endl;
         }
     }
 
-    void insert_before(int x, int pos){
-         Node* node = new Node(x);
-        Node* temp = head;
-        int curr = 0;
-        while(curr < pos-1 && temp -> next){
-            temp = temp -> next;
-            curr++;
-        }
-    }
-
-    void display(){
-        Node* temp = head;
-        while(temp -> next){
-            cout << temp -> data << " -> ";
-            temp = temp -> next;
+    //7
+    void display() {
+        Node *trav = new Node;
+        trav = head;
+        while (trav != NULL) {
+            cout << trav->data << " <-> ";
+            trav = trav->next;
         }
         cout << endl;
     }
 };
 
+int main() {
+    DoublyLinkedList dll;
+
+    int opt;
+    int pos, elem;
+    cout << "Enter the Option:" << endl;
+    cout << "1.Insert at end" << endl;
+    cout << "2.Delete from end" << endl;
+    cout << "3.Insert at Position" << endl;
+    cout << "4.Delete from Position" << endl;
+    cout << "5.Insert after another element" << endl;
+    cout << "6.Insert before another element" << endl;
+    cout << "7.Print the list" << endl;
+    cout << "9.Exit" << endl;
+
+    while (true) {
+        cout << ">Enter Option:  ";
+        cin >> opt;
+        switch (opt) {
+            case 1:
+                cout << "Enter the element to insert:";
+                cin >> elem;
+                dll.insert_end(elem);
+                break;
+            case 2:
+                dll.delete_end();
+                break;
+            case 3:
+                cout << "Enter the location:";
+                cin >> pos;
+                cout << "Enter the element to insert:";
+                cin >> elem;
+                dll.insert_pos(elem, pos);
+                break;
+            case 4:
+                cout << "Enter the location:";
+                cin >> pos;
+                dll.delete_pos(pos);
+                break;
+            case 5:
+                cout << "Enter the element to insert after:";
+                cin >> pos;
+                cout << "Enter the element to insert:";
+                cin >> elem;
+                dll.insert_after(elem, pos);
+                break;
+            case 6:
+                cout << "Enter the element to insert before:";
+                cin >> pos;
+                cout << "Enter the element to insert:";
+                cin >> elem;
+                dll.insert_before(elem, pos);
+                break;
+            case 7:
+                cout << "Linked List : ";
+                dll.display();
+                break;
+            case 9:
+                return 0;
+            default:
+                cout << "Invalid Choice!" << endl;
+        }
+    }
+}
--- Data Structures Lab/Lab 6/Lab6_2.cpp
+++ Data Structures Lab/Lab 6/Lab6_2.cpp
@@ -1,21 +1,62 @@
 #include <iostream>
 using namespace std;
 
-class polynomial{
-    int coeff;
-    int exponent;
-    polynomial *next;
-    polynomial *prev;
-
-    polynomial(int c, int e){
-        coeff = c;
-        exponent = e;
-        next = NULL;
-        prev = NULL;
+class Node {
+   public:
+    int coeff, power;
+    Node *next;
+    Node(int coeff, int power) {
+        this->coeff = coeff;
+        this->power = power;
+        this->next = NULL;
     }
-
 };
 
-int main(){
+void addPolynomials(Node *head1, Node *head2) {
+    if (head1 == NULL && head2 == NULL)
+        return;
+    else if (head1->power == head2->power) {
+        cout << " " << head1->coeff + head2->coeff << "x^" << head1->power << " +";
+        addPolynomials(head1->next, head2->next);
+    } else if (head1->power > head2->power) {
+        cout << " " << head1->coeff << "x^" << head1->power << " +";
+        addPolynomials(head1->next, head2);
+    } else {
+        cout << " " << head2->coeff << "x^" << head2->power << " + ";
+        addPolynomials(head1, head2->next);
+    }
+}
 
+void insert(Node *head, int coeff, int power) {
+    Node *new_node = new Node(coeff, power);
+    while (head->next != NULL) {
+        head = head->next;
+    }
+    head->next = new_node;
+}
+
+void printList(Node *head) {
+    cout << "Polynomial: " << endl;
+    while (head != NULL) {
+        cout << " " << head->coeff << "x"
+             << "^" << head->power;
+        head = head->next;
+        cout << " +";
+    }
+    cout << " 0" << endl;
+}
+
+int main() {
+    Node *head = new Node(5, 2);
+    insert(head, 4, 1);
+    Node *head2 = new Node(6, 2);
+    insert(head2, 4, 1);
+    printList(head);
+    cout << endl;
+    printList(head2);
+    cout << endl << "Addition:" << endl;
+    addPolynomials(head, head2);
+    cout << " 0 " << endl;
+
+    return 0;
 }
